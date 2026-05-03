4cdb95a31ca9f6ab729be821017e3ab952589cc3
S M Nazib Ul Alam <nazib9007.ice.bup@gmail.com>
2026-05-04 00:42:14 +0600

    Add files via upload

---
+++ constructor.js
@@ -0,0 +1,8 @@
+const minimum = Math.min(45, 21, 65, 99, 126, 5, -2);
+console.log(minimum);
+
+const maximum = Math.max(45, 21, 65, 99, 126, 5, -2);
+console.log(maximum);
+
+const pie = Math.PI;
+console.log(pie);
