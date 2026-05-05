517424cb9548bf7c4922cf4141fab7aafe121d08
S M Nazib Ul Alam <nazib9007.ice.bup@gmail.com>
2026-05-05 23:19:13 +0600

    Update constructor.js
    Added additional math functions and comments for clarity.

--- constructor.js
+++ constructor.js
@@ -1,8 +1,14 @@
-const minimum = Math.min(45, 21, 65, 99, 126, 5, -2);
+const minimum = Math.min(45, 21, 65, 99, 126, 5, -2); //find min number...
 console.log(minimum);
 
-const maximum = Math.max(45, 21, 65, 99, 126, 5, -2);
+const maximum = Math.max(45, 21, 65, 99, 126, 5, -2); //find max number..
 console.log(maximum);
 
-const pie = Math.PI;
+const pie = Math.PI;//  Pie value..
 console.log(pie);
+
+console.log(Math.abs(-7));// abs() always gives positive value if pass negeative value also return positive value like example..
+console.log(Math.round(5.6));//round provide complete number not provide fraction number..
+console.log(Math.floor(5.7)); //floor provide near number..
+console.log(Math.ceil(4.7)); //ceil also provide near positive value..
+console.log(Math.random()); //random() provide any random number between 0 to 1;
